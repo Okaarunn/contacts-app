9c4b701172fae6aee2822b1799a1bf9b9f65e53d
AdityaWjy <adamfadzal662@gmail.com>
2025-09-17 19:38:59 +0700

    feat: add contact list

--- src/ContactApp.jsx
+++ src/ContactApp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Contacts } from "./data.js";
 import ContactList from "./ContactList.jsx";
+import ContactInput from "./ContactInput.jsx";
 
 class ContactApp extends React.Component {
   constructor(props) {
@@ -10,12 +11,29 @@ class ContactApp extends React.Component {
       contacts: Contacts(),
     };
 
-    // set ondelete
+    // set bind event handler
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onAddContactHandler = this.onAddContactHandler.bind(this);
+  }
+
+  // add contact function
+  onAddContactHandler({ name, tag }) {
+    this.setState((prevState) => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          {
+            id: +new Date(),
+            name,
+            tag,
+            imgURL: "default.jpg",
+          },
+        ],
+      };
+    });
   }
 
   // ondelete method
-
   onDeleteHandler(id) {
     const contacts = this.state.contacts.filter((contact) => contact.id !== id);
     this.setState({ contacts });
@@ -25,6 +43,7 @@ class ContactApp extends React.Component {
     return (
       <div className="contact-app">
         <h1>Daftar kontak</h1>
+        <ContactInput addContact={this.onAddContactHandler} />
         <ContactList
           contacts={this.state.contacts}
           onDelete={this.onDeleteHandler}
